dc4fbb389a915657244aaa83a73a50f63ed798b0
ibouelezz <ibrahim.ali.aboelezz@gmail.com>
2023-12-27 13:50:31 +0200

    create-react-app boilerplate, html, css boilerplate migration, cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>
+                The Great
+                <br /> <span id="colorDisplay">RGB</span> <br />
+                Color Game
+            </h1>
+
+            <div id="curtain">
+                <button>New Colors</button>
+                <span id="message"></span>
+                <button class="mode">Easy</button>
+                <button class="mode selected">Hard</button>
+            </div>
+
+            <div id="container">
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+            </div>
+        </>
+    );
 }
 
 export default App;
